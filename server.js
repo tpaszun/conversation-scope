77dd43c0e0f1fb94205d491a77a0da198896c022
Unknown <andrzej1_1@o2.pl>
2017-11-25 22:24:53 +0100

    Add basic implementation

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var ConversationScope = require('./index.js');
 var express = require('express')
 var NodeSession = require('node-session');
 var path = require('path');
@@ -9,16 +10,24 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(function (req, res, next) {
-    session.startSession(req, res, function() {
-        req.session.put('views', (req.session.get('views') || 0) + 1)
-        next()
-    })
+    session.startSession(req, res, next);
+});
+
+app.use(function (req, res, next) {
+    ConversationScope.init(req, res, next);
+});
+
+app.use(function (req, res, next) {
+    req.session.begin();
+    req.session.put('views', (req.session.get('views') || 0) + 1)
+    next()
 })
 
 app.get('/', function (req, res, next) {
     res.render('index', {
-        number_views: req.session.get('views')
+        number_views: req.session.get('views'),
+        cid: req.session.generateCid()
     });
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!\n'))
