041c6032d773e304f447132e1a6721933c9cf99e
Unknown <andrzej1_1@o2.pl>
2018-01-03 23:15:59 +0100

    Add number guess example

--- examples/viewsCounter/index.js
+++ examples/viewsCounter/index.js
@@ -9,6 +9,8 @@ var app = express()
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(function (req, res, next) {
     session.startSession(req, res, next);
 });
